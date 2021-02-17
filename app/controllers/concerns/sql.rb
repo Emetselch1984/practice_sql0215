f235b98f17b7e024a013aafe880d4fee1d8890b7
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-17 14:45:56 +0900

    Before check

---
+++ app/controllers/concerns/sql.rb
@@ -0,0 +1,24 @@
+module Sql
+  extend ActiveSupport::Concern
+  SumSql = <<~SQL
+      SELECT SUM(amount) FROM orders;
+  SQL
+  SumSql2 = <<~SQL
+      SELECT SUM(amount) FROM ORDERS WHERE order_time >= '2017-01-01 00:00:00' AND order_time < '2017-02-01 00:00:00';
+  SQL
+  AvgSql = <<~SQL
+      SELECT AVG(price) FROM products;
+  SQL
+  MinSql = <<~SQL
+      SELECT MIN(price) FROM products;
+  SQL
+  MaxSql = <<~SQL
+      SELECT MAX(price) FROM products;
+  SQL
+  CountUser = <<~SQL
+      select count(distinct user_id) from access_logs where request_month = '2017-01-01'
+  SQL
+  PrefectureCount = <<~SQL
+      select prefecture_id,count(*) from users group by prefecture_id;
+  SQL
+end
--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -1,41 +1,22 @@
 # frozen_string_literal: true
 
 class OutputsController < ApplicationController
+    include Sql
   def index
-    sql = <<~SQL
-      SELECT SUM(amount) FROM orders;
-    SQL
-    sql2 = <<~SQL
-      SELECT SUM(amount) FROM ORDERS WHERE order_time >= '2017-01-01 00:00:00' AND order_time < '2017-02-01 00:00:00';
-    SQL
-    sql3 = <<~SQL
-      SELECT AVG(price) FROM products;
-    SQL
-    sql4 = <<~SQL
-      SELECT MIN(price) FROM products;
-    SQL
-    sql5 = <<~SQL
-      SELECT MAX(price) FROM products;
-    SQL
-    sql6 = <<~SQL
-      select count(distinct user_id) from access_logs where request_month = '2017-01-01'
-    SQL
-    sql7 = <<~SQL
-      select prefecture_id,count(*) from users group by prefecture_id;
-    SQL
-    result = Order.connection.select_all(sql).to_a
+
+    result = Order.connection.select_all(SumSql).to_a
     @sum = result[0]['SUM(amount)']
-    result2 = Order.connection.select_all(sql2).to_a
+    result2 = Order.connection.select_all(SumSql2).to_a
     @sum2 = result2[0]['SUM(amount)']
-    result3 = Product.connection.select_all(sql3).to_a
+    result3 = Product.connection.select_all(AvgSql).to_a
     @avg = result3[0]['AVG(price)']
-    result4 = Product.connection.select_all(sql4).to_a
+    result4 = Product.connection.select_all(MinSql).to_a
     @minPrice = result4[0]['MIN(price)']
-    result5 = Product.connection.select_all(sql5).to_a
+    result5 = Product.connection.select_all(MaxSql).to_a
     @maxPrice = result5[0]['MAX(price)']
     @userCount = User.all.size
-    result6 = AccessLog.connection.select_all(sql6).to_a
+    result6 = AccessLog.connection.select_all(CountUser).to_a
     @accesslog = result6[0]['count(distinct user_id)']
-      @prefectures = User.connection.select_all(sql7).to_a
+      @prefectures = User.connection.select_all(PrefectureCount).to_a
   end
 end
